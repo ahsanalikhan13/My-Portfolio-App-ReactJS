5d78616e46034d65e945a5780ae6b0683f51b97c
ahsanalikhan13 <aahsanalikhan.13@gmail.com>
2023-02-24 15:38:48 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Header from './components/Header';
 import Home from './components/Home';
-// import Footer from './components/Footer';
-
 
 function App() {
   return <>
-    <Header/>
-    <Home/>
-    {/* <Footer/> */}
+  <Header/>
+  <Home/>
   </>
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import {motion} from 'framer-motion';
-import Typewriter from 'typewriter-effect';
-import {BsArrowUpRight, BsChevronDown} from 'react-icons/bs';
-import ahsan from '../assets/img1.png';
-
 
 const Home = () => {
 
@@ -16,71 +12,20 @@ const Home = () => {
                 whileInView:{
                     x: 0,
                     opacity: 1,
-                }          
-        },
-
-        button:{
-          initial: {
-              y:"-100%",
-              opacity: 0,
-          },
-              whileInView:{
-                  y: 0,
-                  opacity: 1,
-              }          
+                }
+            
         }
     }
-
   return (
     <div id='home'>
       <section>
         <div>
             <motion.h1 initial {...animations.h1}>
-
                 Hi, I am <br/> AHSAN ALI KHAN
-
             </motion.h1>
-
-            <Typewriter
-            options={{
-              strings: ["A React Developer", "JavaScript Developer", "Scrum Master"],
-              autoStart: true,
-              loop: true,
-              cursor: null,
-              wrapperClassName:"typewriterpara",
-            }}
-            />
-
-            <div>
-            <a href="mailto:aahsanalikhan.13@gmail.com">Hire Me!</a>
-            <a href="#work">Projects! <BsArrowUpRight/></a>
-            </div>
-            
-            <article>
-              <p>+<span>100</span></p>
-              <span>Clients WorldWide!</span>
-            </article>
-
-            <aside>
-            <article>
-              <p>+<span>500</span></p>
-              <span>Projects Accomplished!</span>
-            </article>
-
-            <article data-special>
-              <p>Contact</p>
-              <span>aahsanalikhan.13@gmail.com</span>
-            </article>
-            </aside>
         </div>
       </section>
-
-      <section>
-        <img src={ahsan} alt="ahsan" />
-      </section>
-
-      <BsChevronDown/>
     </div>
   )
 }
-export default Home;
+export default Home
