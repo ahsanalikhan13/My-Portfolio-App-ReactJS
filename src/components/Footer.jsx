50090b2b61c1a55004a3b804bd21943155cc0d75
ahsanalikhan13 <aahsanalikhan.13@gmail.com>
2023-02-27 14:45:37 +0500

    second commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {AiFillInstagram} from 'react-icons';
+
+const Footer = () => {
+  return (
+   <footer>
+    <a href="https://www.instagram.com/">
+      <AiFillInstagram/>
+    </a>
+   </footer>
+  )
+}
+
+export default Footer;
