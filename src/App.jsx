24e962737bbe9f8491a8f543deb9928441bca981
ahsanalikhan13 <aahsanalikhan.13@gmail.com>
2023-03-09 15:21:24 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import Header from './components/Header';
 import Home from './components/Home';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 37a831e (first commit)
-=======
->>>>>>> 69036f8 (resolve conflicts)
+
 import Timeline from "./components/Timeline";
 import Work from "./components/Work";
 // import Footer from './components/Footer';
 
-<<<<<<< HEAD
 
 function App() {
   return <>
@@ -21,26 +14,7 @@ function App() {
     <Work/>
     <Timeline/>
     {/* <Footer/> */}
-=======
 
-function App() {
-  return <>
-  <Header/>
-  <Home/>
->>>>>>> 6aaf2e3 (resolve conflicts)
-<<<<<<< HEAD
-=======
-
-function App() {
-  return <>
-    <Header/>
-    <Home/>
-    <Work/>
-    <Timeline/>
-    {/* <Footer/> */}
->>>>>>> 37a831e (first commit)
-=======
->>>>>>> 69036f8 (resolve conflicts)
   </>
 }
 
